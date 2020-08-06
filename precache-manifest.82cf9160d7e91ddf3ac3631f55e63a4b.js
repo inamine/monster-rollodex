8575f9419b68534a6caffe9657652e0a664a5af1
Renato Inamine <renato.inamine@gmail.com>
2020-08-06 19:00:01 -0300

    Updates

---
+++ precache-manifest.82cf9160d7e91ddf3ac3631f55e63a4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092dadbeb35e367cb0e05dbf661e476e",
+    "url": "/monster-rollodex/index.html"
+  },
+  {
+    "revision": "447880f04141e4cd2bc6",
+    "url": "/monster-rollodex/static/css/main.49690c01.chunk.css"
+  },
+  {
+    "revision": "e4cfa178a7b73f3a3d39",
+    "url": "/monster-rollodex/static/js/2.289458fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rollodex/static/js/2.289458fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447880f04141e4cd2bc6",
+    "url": "/monster-rollodex/static/js/main.c34cbf0f.chunk.js"
+  },
+  {
+    "revision": "239fc18b80e4b39ec3eb",
+    "url": "/monster-rollodex/static/js/runtime-main.234bb251.js"
+  }
+]);
