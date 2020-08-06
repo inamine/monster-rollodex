e6f1564d2997e90665f94671635ed88ed6131e84
Renato Inamine <renato.inamine@gmail.com>
2020-08-06 18:59:06 -0300

    include files

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
       monster.name.toLowerCase().includes(searchField.toLowerCase()))
    return (
       <div className="App">
+      <h1> Monsters Rolodex </h1>
         <SearchBox placeholder="Search Monster" handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters}/>
       </div>
